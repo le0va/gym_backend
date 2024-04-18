311bacf6b420752d9e89e556f2956ca24e898c4f
leova <leovkin2003@gmail.com>
2024-04-18 14:56:53 +0300

    ref: Removed userRoles from accessToken

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,23 +1,22 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import UserPublicJwtDto from '../dto/user-public-jwt.dto';
 import { ConfigService } from '@nestjs/config';
-
+import UserPublicJwtDto from '../dto/user-public-jwt.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
-        private readonly configService: ConfigService
+        private readonly configService: ConfigService,
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
+            ignoreExpiration: true,
             secretOrKey: configService.get('JWT_SECRET')
         });
     }
 
     async validate(payload: UserPublicJwtDto) {
-        return { id: payload.sub, roles: payload.roles };
+        return { id: payload.sub };
     }
 }
