1cc04da6ae79853af6f8b0df326ca7898fe5ec70
leova <leovkin2003@gmail.com>
2024-04-18 14:53:59 +0300

    ref: Added dependency for usidng RolesGuard

--- src/training/training.module.ts
+++ src/training/training.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrainingController } from './training.controller';
 import { TrainingService } from './training.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import User from 'src/users/users.entity';
 import TrainingSession from './training-sessions.entity';
+import { UsersModule } from 'src/users/users.module';
+import User from 'src/users/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, TrainingSession])],
+  imports: [TypeOrmModule.forFeature([User, TrainingSession]), UsersModule],
   controllers: [TrainingController],
   providers: [TrainingService]
 })
