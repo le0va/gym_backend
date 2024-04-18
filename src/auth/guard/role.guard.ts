ec5c6370c0d2b813a9c1406a0a0d73875eb68f20
leova <leovkin2003@gmail.com>
2024-04-18 14:58:16 +0300

    ref: Added fetching userRoles from service

--- src/auth/guard/role.guard.ts
+++ src/auth/guard/role.guard.ts
@@ -1,14 +1,18 @@
-import { CanActivate, ExecutionContext, Type } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Inject, Type } from "@nestjs/common";
+import { UsersService } from "src/users/users.service";
 import ReqWithUser from "../interface/req-with-user.interface";
 import Role from "src/users/role.enum";
 
 const RoleGuard = (role: Role): Type<CanActivate> => {
     class RoleGuardGeneric implements CanActivate {
-        canActivate(context: ExecutionContext) {
+        constructor(@Inject(UsersService) private usersService: UsersService) { }
+
+        async canActivate(context: ExecutionContext): Promise<boolean> {
             const request = context.switchToHttp().getRequest<ReqWithUser>();
             const user = request.user;
 
-            return user?.roles.includes(role);
+            const { roles } = await this.usersService.getById(user.id);
+            return roles.includes(role);
         }
     }
     return RoleGuardGeneric;
